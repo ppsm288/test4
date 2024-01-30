597fa1a7c7a926883ee70db3e04fdc3cfc969c1e
ppsm288 <ppsm288@gmail.com>
2024-01-30 13:40:33 -0600

    changes

--- hello.java
+++ hello.java
@@ -1,12 +1,12 @@
-public class Hello {
+public class HelloWorld {
     public static void main(String[] args) {
-        // Define the number of times to print "Hello, World!"
-        int timesToPrint = 5;
-        
-        // Loop to print "Hello, World!" multiple times
-        for (int i = 0; i < timesToPrint; i++) {
-            System.out.println("Hello, World!");
-        }
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.printHelloWorld();
+    }
+
+    public void printHelloWorld() {
+        System.out.println("Hello, World!");
     }
 }
+
 
