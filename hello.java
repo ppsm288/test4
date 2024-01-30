306645bdbfb5acc2930a2da6a4b9abd134c236a8
ppsm288 <ppsm288@gmail.com>
2024-01-30 13:28:07 -0600

    changed code

--- hello.java
+++ hello.java
@@ -1,5 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Define the number of times to print "Hello, World!"
+        int timesToPrint = 5;
+        
+        // Loop to print "Hello, World!" multiple times
+        for (int i = 0; i < timesToPrint; i++) {
+            System.out.println("Hello, World!");
+        }
     }
 }
+
